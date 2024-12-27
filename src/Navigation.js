2a6877c9fe4e808d739450cc859048772a3f8ef7
danielstet <stetsyukdaniel@gmail.com>
2024-12-27 12:45:09 +0200

    edited

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,24 +1,23 @@
 import { NavLink } from 'react-router-dom';
 
 export const Navigation = ({ children }) => {
-  return (
-    <nav className="col-span-3 flex flex-col border-r-2 border-yellow-400 h-full">
-      {children}
-    </nav>
-  );
+	return (
+		<nav className='col-span-3 flex flex-col border-r-2 border-indigo-400 h-full'>
+			{children}
+		</nav>
+	);
 };
 
 export const NavigationLink = ({ children, to }) => {
-  return (
-    <NavLink
-      to={to}
-      className={({ isActive }) =>
-        isActive
-          ? 'bg-yellow-300 hover:bg-yellow-400'
-          : 'bg-white hover:bg-yellow-100'
-      }
-    >
-      <article className="w-full p-4">{children}</article>
-    </NavLink>
-  );
+	return (
+		<NavLink
+			to={to}
+			className={({ isActive }) =>
+				isActive
+					? 'bg-indigo-300 hover:bg-indigo-400'
+					: 'bg-white hover:bg-indigo-100'
+			}>
+			<article className='w-full p-4'>{children}</article>
+		</NavLink>
+	);
 };
